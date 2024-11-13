8637f7376c3b0f34cdaa4d1c6762ecae4551b6d2
sxo-mpa <marcus.patam@sixense-group.com>
2024-11-13 13:12:01 +1100

    Updates

---
+++ _app/immutable/chunks/paths.D8JR27av.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_12qs95e)==null?void 0:s.base)??"/position-app";var e;const t=((e=globalThis.__sveltekit_12qs95e)==null?void 0:e.assets)??a;export{t as a,a as b};
